510c88dd558e23e4b3f2c182d865db7a2611620e
gong.zhangxun <gong.zhangxun@olaola.chat>
2022-08-08 19:32:03 +0800

    init

---
+++ generate.go
@@ -0,0 +1,5 @@
+//go:generate rm -rf /Users/gongzhangxun/my
+//go:generate mkdir -p /Users/gongzhangxun/my
+//go:generate touch /Users/gongzhangxun/my/deploy.sh
+
+package generate
---
+++ go.mod
@@ -0,0 +1,3 @@
+module  github.com/gongzhxu/generate
+
+go 1.18
